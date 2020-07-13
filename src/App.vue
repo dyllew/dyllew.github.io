e1c84bbfd1aa2dc26b7f4f939d20fa037a5fdd73
Dylan Lewis <dylanlewis@beepboopbopmachine.lan>
2020-07-13 07:58:28 -0500

    Getting started

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <RootContainer/>
+  </div>
+</template>
+
+<script>
+import RootContainer from './components/RootContainer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RootContainer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background:
+  margin-top: 60px;
+}
+</style>
