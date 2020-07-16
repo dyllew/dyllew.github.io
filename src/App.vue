4239806fd676edc1a230f3672296b08ac9a621cb
Dylan Lewis <dylanlewis@beepboopbopmachine.lan>
2020-07-15 23:32:18 -0500

    added more to about me page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <div id="app" class="container-fluid">
-    <div id="site-header" class="row">
-      <h1 id="name"> Dylan Lewis </h1>
-      <div id="nav-bar">
-        <router-link class="router-link" to="/about">About</router-link> |
-        <router-link class="router-link" to="/projects">Projects</router-link> |
-        <router-link class="router-link" to="/artwork">Artwork</router-link> |
-        <router-link class="router-link" to="/resume">Resume</router-link>
+  <div id="app" class="container-fluid d-flex flex-column mh-100">
+    <div class="row justify-content-center">
+      <div class="col page-header">
+        <div class="row justify-content-center">
+          <h1 id="name"> Dylan Lewis </h1>
+          <!-- <span style="font-size: 3em; color:Mediumslateblue ;">
+            <i class="fab fa-linkedin"></i>
+          </span> -->
+        </div>
+        <div id="nav-bar" class="row justify-content-center">
+          <router-link class="router-link" to="/about">About</router-link> |
+          <router-link class="router-link" to="/projects">Projects</router-link> |
+          <router-link class="router-link" to="/artwork">Artwork</router-link> |
+          <router-link class="router-link" to="/resume">Resume</router-link>
+        </div>
       </div>
     </div>
-    <router-view></router-view>
+    <div class="row flex-grow-1">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -23,9 +32,6 @@ export default {
 </script>
 
 <style>
-body {
-  margin: 0 0 0 0;
-}
 
 .router-link {
   color: turquoise;
@@ -54,4 +60,27 @@ body {
   font-family: Helvetica, Arial, sans-serif;
   font-size: 20px;
 }
+
+html {
+  font-size: 1rem;
+}
+
+@include media-breakpoint-up(sm) {
+  html {
+    font-size: 1.2rem;
+  }
+}
+
+@include media-breakpoint-up(md) {
+  html {
+    font-size: 1.4rem;
+  }
+}
+
+@include media-breakpoint-up(lg) {
+  html {
+    font-size: 1.6rem;
+  }
+}
+
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about container-fluid">
+  <div class="about row h-100 align-items-center">
+      <div class="col">
+        <img 
+        id="leo-and-dylan-pic"
+        src="../../public/assets/leo_n_me.png" 
+        class="rounded img-fluid"
+        alt="Leo and Dylan" />
+      </div>
+      <div id="about-description" class="col">
+        <p>Hey there! I'm a Masters student at MIT studying
+        Computer Science. But more importantly, I am a proud dog dad
+        to my son, Leo 🐶. I'm interested in Software Engineering, Data Science & Machine Learning, and Web Development! You can contact through email, add me on LinkedIn & GitHub!</p> 
+      </div>
+
   </div>    
 </template>
 
@@ -13,4 +26,14 @@ export default {
 </script>
 
 <style>
+#leo-and-dylan-pic  {
+  border: 4px solid turquoise;
+}
+
+#about-description {
+  font-family: Georgia, Cambria, Cochin, Times, 'Times New Roman', serif;
+  font-size: 30px;
+  font-size-adjust: 0.5;
+}
+
 </style>
