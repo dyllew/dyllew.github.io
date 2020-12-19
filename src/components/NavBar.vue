bb14a33d6d8005b7ebdcbe6773a7e110dbb337de
Dylan Lewis <dylanlewis@beepboopbopmachine.local>
2020-12-19 02:46:56 -0500

    deploy

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row pt-2 justify-content-center">
       <div id="nav-bar" class="col-md-12">
-          <router-link class="router-link" to="/about">About</router-link> |
+          <router-link class="router-link" to="/about">About Me</router-link> |
           <router-link class="router-link" to="/projects">Projects</router-link> |
           <router-link class="router-link" to="/artwork">Artwork</router-link> |
           <router-link class="router-link" to="/resume">Resume</router-link>
